918dc4b11c61dd555d9a935439815fafc4c6610d
unknown <pedro.maia@DEVEL1-PC.dialhost.bh.local>
2015-01-30 15:05:14 -0200

    Add alternative environment detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
+$env = $app->detectEnvironment(function ()
+{
 
-	'local' => array('homestead'),
-
-));
+	return require __DIR__.'/environment.php';
+});
 
 /*
 |--------------------------------------------------------------------------
