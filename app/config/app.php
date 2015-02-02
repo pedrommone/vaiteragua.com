55634800cf653b3360709f1c27f15ec6ac2886f9
Pedro Maia <pedro@pedromm.com>
2015-02-02 16:20:45 -0200

    Added Dingo

--- app/config/app.php
+++ app/config/app.php
@@ -121,6 +121,7 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
+		'Dingo\Api\Provider\ApiServiceProvider'
 
 	),
 
@@ -188,6 +189,7 @@ return array(
 		'URL'               => 'Illuminate\Support\Facades\URL',
 		'Validator'         => 'Illuminate\Support\Facades\Validator',
 		'View'              => 'Illuminate\Support\Facades\View',
+		'API'               => 'Dingo\Api\Facade\API'
 
 	),
 
