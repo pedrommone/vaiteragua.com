ad284a072fd627b344208f7e763e54bb6bf0028f
magdielduarte <magdiel.kesller@gmail.com>
2015-02-04 01:30:20 -0200

    effect drop js

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -11,9 +11,20 @@
 		<p>Se todo mundo colaborar não vai faltar pra ninguém</p>
 
 		<section class="water">
-			<div class="drop">100%</div>
+			<div class="mousedrop">
+				<div class="drop">100%</div>
+			</div>
 			<a href="#" class="btn">Ver por reservatórios</a>
 		</section>
 		<div class="ruler"></div>
+
+		<script>
+				var mousedrop = document.querySelector('.mousedrop'),
+						drop =  document.querySelector('.drop');
+				
+				mousedrop.onmousemove = function() {
+					drop.style.left = window.event.clientX + 'px';
+				};
+		</script>
 </body>
 </html>
