b4636d12251e538e2b8900abeaef084f220e7293
Pedro Maia <pedro@pedromm.com>
2015-02-05 10:13:27 -0200

    Removido canvas

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -8,20 +8,18 @@
 	</head>
 
 	<body>
-			<img src="{{ asset('assets/img/vaiteragua.png') }}" class="brand" alt="">
-			<p>Se todo mundo colaborar não vai faltar pra ninguém</p>
+		<img src="{{ asset('assets/img/vaiteragua.png') }}" class="brand" alt="">
+		<p>Se todo mundo colaborar não vai faltar pra ninguém</p>
 
-			<water>
-				<div class="mousedrop">
-					<div class="drop">100%</div>
-				</div>
+		<section class="water">
+			<div class="mousedrop">
+				<div class="drop">100%</div>
+			</div>
 
-				<a href="#" class="btn">Ver por reservatórios</a>
-			</water>
+			<a href="#" class="btn">Ver por reservatórios</a>
+		</section>
 
-			<div class="ruler"></div>
-
-			<script src="{{ asset('assets/js/wave.js') }}"></script>
-			<script src="{{ asset('assets/js/main.js') }}"></script>
+		<div class="ruler"></div>
+		<script src="{{ asset('assets/ja/main.js') }}"></script>
 	</body>
 </html>
