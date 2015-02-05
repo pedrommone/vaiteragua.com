8f5b90757dc02efac164460ed33590951969d379
Pedro Maia <pedro@pedromm.com>
2015-02-05 14:12:08 -0200

    Added favicon

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -6,6 +6,9 @@
 
 		<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Play" type="text/css">
 		<link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+		<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+		<link rel="icon" href="/favicon.ico" type="image/x-icon">
 	</head>
 
 	<body>
