7737a23f3efa8b5f69eb5213b72c342d7d38fe87
Pedro Maia <pedro@pedromm.com>
2015-02-05 09:55:59 -0200

    Added initial canvas

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 	<head>
 		<meta charset="UTF-8">
 		<title>Vai ter água</title>
@@ -11,30 +11,17 @@
 			<img src="{{ asset('assets/img/vaiteragua.png') }}" class="brand" alt="">
 			<p>Se todo mundo colaborar não vai faltar pra ninguém</p>
 
-			<section class="water">
+			<water>
 				<div class="mousedrop">
 					<div class="drop">100%</div>
 				</div>
+
 				<a href="#" class="btn">Ver por reservatórios</a>
-			</section>
+			</water>
 
 			<div class="ruler"></div>
 
-			<script>
-				var mousedrop = document.querySelector('.mousedrop')
-					, drop =  document.querySelector('.drop');
-				
-				mousedrop.onmousemove = function() {
-					drop.style.left = window.event.clientX + 'px';
-				};
-
-				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-				})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-				ga('create', 'UA-59389995-1', 'auto');
-				ga('send', 'pageview');
-			</script>
+			<script src="{{ asset('assets/js/wave.js') }}"></script>
+			<script src="{{ asset('assets/js/main.js') }}"></script>
 	</body>
 </html>
