9eb8df611b16c3ba8f3dfd1a15a673818b5125ca
Pedro Maia <pedro@pedromm.com>
2015-02-09 22:53:50 -0200

    Removed spaces

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -10,8 +10,6 @@
 		<link rel="shortcut icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 		<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
 
-
-		
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 
 		<meta property="og:type" content="website" />
