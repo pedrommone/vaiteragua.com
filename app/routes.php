2db3c34be2400ca79bb4945bc1f23802013fee7a
Pedro Maia <pedro@pedromm.com>
2015-02-23 16:21:53 -0300

    Added WhatsApp API

--- app/routes.php
+++ app/routes.php
@@ -56,3 +56,21 @@ Route::group(['prefix' => 'api'], function()
 		});
 	});
 });
+
+Route::get('wpp', function()
+{
+
+	$username = "14387001374";
+	$identity = "%f1%e9%12%1f%b3%f1m%cc%dc%7b%14j%99m%f0%3d%11%06%b3%2e";
+	$nickname = "Vai ter agua";
+	$password = "jrotTvQe4sfZpB5d6rCUDXPDv7g=";
+	$debug = true;
+
+	// Create a instance of WhastPort.
+	$w = new WhatsProt($username, $identity, $nickname, $debug);
+
+	$w->connect(); // Connect to WhatsApp network
+	$w->loginWithPassword($password); // logging in with the password we got!
+
+	$w->sendMessage("553199821215" , "Eae babaca.");
+});
